90bbc967d37fd7c04ee3c239d58f93ac79580e5d
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-20 09:07:36 +0400

    add Task 2 - convert array of string to HashMap, the item of array will be the key and the length of the item will be the value

---
+++ src/Task2WordLen.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task2WordLen {
+    public static void main(String[] args){
+        String[] array = {"Aa","BbB","C","D"};
+        System.out.println(wordLen(array));
+    }
+    public static Map<String, Integer> wordLen(String[] array){
+        Map<String, Integer> map = new HashMap<>();
+        for(String result : array){
+            map.put(result,result.length());
+        }
+        return map;
+    }
+}
