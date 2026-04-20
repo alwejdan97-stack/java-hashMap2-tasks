27222a7e742f8d26e5b46da3b718a1478d9777c7
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-20 09:19:15 +0400

    add Task 2 - convert array of string to HashMap, the first character of item will be the key and the last character of the string will be the value

---
+++ src/Task3Pairs.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task3Pairs {
+    public static void main(String[] args){
+        String[] array = {"Hoor","Ali","Sara","Wejdan"};
+        System.out.println(pairs(array));
+    }
+    public static Map<String, String> pairs(String[] array){
+        Map<String, String> map= new HashMap<>();
+        for(String result: array){
+            String key= result.substring(0,1);
+            String value= result.substring(result.length()-1);
+            map.put(key, value);
+        }
+        return map;
+    }
+}
