1d577cb8761cb63a23aef0c0561efc9183ddf348
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-20 10:29:56 +0400

    add Task 5 - convert array of string to HashMap, the first character will be the key and the other same string or the whole string the string will be the value

---
+++ src/Task5FirstChar.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task5FirstChar {
+    public static void main(String[] args){
+        String[] array= {"A","B","C","D","aA","DdD","Aa"};
+        String[] array1= {};
+        System.out.println(firstChar(array));
+        System.out.println(firstChar(array1));
+    }
+    public static Map<String,String> firstChar(String[] array){
+        Map<String,String> map= new HashMap<>();
+        for(String result:array){
+            String key= result.substring(0,1);
+            if(map.containsKey(key)){
+                map.put(key, map.get(key)+result);
+            }else{
+                map.put(key,result);
+            }
+        }
+        return map;
+    }
+}
