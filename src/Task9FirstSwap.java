7557e5eac83a076566854771586ea3e9af95be44
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-20 12:43:35 +0400

    add Task 9 - if 2 string share same first character, swap them allowed for one time only

--- src/Task9FirstSwap.java
+++ src/Task9FirstSwap.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
 
 public class Task9FirstSwap {
+    public static void main(String[] args){
+        String[] array={"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"};
+        System.out.println(Arrays.toString(firstSwap(array)));
+    }
+    public static String[] firstSwap(String[] array){
+        Map<String, Integer> map=new HashMap<>();
+        for(int i=0; i<array.length; i++){
+            String key=array[i].substring(0,1);
+            if(map.containsKey(key)){
+                int before=map.get(key);
+                if(before!=-1){
+                    String first = array[i];
+                    array[i] = array[before];
+                    array[before] = first;
+                    map.put(key,-1);
+                }
+            }else{
+                map.put(key,i);
+            }
+        }
+        return array;
+    }
 }
