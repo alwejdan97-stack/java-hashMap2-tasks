1105c903035bd5c3ce86f2f883f3f64505d50c19
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-20 08:54:49 +0400

    add Task 1 - convert array of string to HashMap, the item of array will be the key and 0 will be the value

---
+++ src/Task1Word0.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task1Word0 {
+    public static void main(String[] args){
+        String[] array= {"A","B","C","D"};
+        System.out.println(word0(array));
+    }
+    public static Map<String, Integer> word0(String[] array){
+        Map<String, Integer> map= new HashMap<>();
+        for(String result: array){
+            map.put(result, 0);
+        }
+        return map;
+    }
+}
