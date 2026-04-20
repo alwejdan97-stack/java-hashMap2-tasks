18cbe6f7620ba1aef3ab0c2c7ce9361afa85c2c1
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-20 13:00:16 +0400

    add Task 6 - return string only if the item appear even times not odd(2, 4, 6, .....)

--- src/Task6WordAppend.java
+++ src/Task6WordAppend.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Task6WordAppend {
+    public static void main(String[] args){
+        String[] array= {"a", "b", "a","a"};
+        String[] array2= {"a", "b", "a", "c", "a", "d", "a"};
+        String[] array3= {"a","a"," "};
+        System.out.println(wordAppend(array));
+        System.out.println(wordAppend(array2));
+        System.out.println(wordAppend(array3));
+    }
+    public static String wordAppend(String[] array){
+        Map<String,Integer> map= new HashMap<>();
+        String New="";
+        int count;
+        for(String result: array){
+            count = map.getOrDefault(result,0)+1;
+            map.put(result,count);
+            if(count %2==0){
+                New = New+ result;
+            }
+        }
+        return New;
+    }
 }
