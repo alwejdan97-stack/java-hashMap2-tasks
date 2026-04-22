b2b5f040e5f3c060fd833a2d2706ad7b5e085fa3
wejdan al-subhi <alwejdan97@gmail.com>
2026-04-22 14:24:45 +0400

    add Task 4 - convert array of string to HashMap, the item will be the key and the count of how many appearance of the string will be the value

--- src/wordCount.java
+++ src/wordCount.java
@@ -1,12 +1,12 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class Task4WordCoun {
+public class wordCount {
     public static void main(String[] args){
         String[] array= {"A","B","C","D","A","D"};
-        System.out.println(wordCoun(array));
+        System.out.println(wordCount(array));
     }
-    public static Map<String, Integer> wordCoun(String[] array){
+    public static Map<String, Integer> wordCount(String[] array){
         Map<String, Integer> map= new HashMap<>();
         for(String result: array){
             if(map.containsKey(result)){
